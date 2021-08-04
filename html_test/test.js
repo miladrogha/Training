41b8e837ce571c6247d9e3ff59785ed8c61da609
Milad Rogha <60856402+miladrogha@users.noreply.github.com>
2021-08-04 16:40:53 -0400

    updated for server run

--- html_test/test.js
+++ html_test/test.js
@@ -1,5 +1,5 @@
 async function drawLineChart(){
-    const dataset = await d3.json("nyc_weather_data.json")
+    const dataset = await d3.json("http://localhost:8080/nyc_weather_data.json")
     console.log(dataset)
 }
 
