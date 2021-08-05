ea39fe3c8e0423c936b7421b366730a4af829b73
Milad Rogha <60856402+miladrogha@users.noreply.github.com>
2021-08-05 01:33:10 -0400

    miss spelling resolved

--- html_test/test.js
+++ html_test/test.js
@@ -32,32 +32,32 @@ async function drawLineChart(){
             dimensions.margin.top
         }px)` )
     
-        const yScale = d3.scaleLinear()
-            .domain(d3.extent(dataset,yAccessor))
-            .range([dimensions.boundedHeight,0])
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(dataset,yAccessor))
+        .range([dimensions.boundedHeight,0])
         
-            const freezingTemperaturePlacement = yscale(32)
-            const freezingTemaratures = bounds.append("rect")
-                .attr("x", 0)
-                .attr("width", dimensions.boundedWidth)
-                .attr("y", freezingTemperaturePlacement)
-                .attr("height", dimensions.boundedHeight
-                    - freezingTemperaturePlacement)
-                .attr("fill", "#e0f3f3")
+    const freezingTemperaturePlacement = yScale(32)
+    const freezingTemaratures = bounds.append("rect")
+        .attr("x", 0)
+        .attr("width", dimensions.boundedWidth)
+        .attr("y", freezingTemperaturePlacement)
+        .attr("height", dimensions.boundedHeight
+            - freezingTemperaturePlacement)
+        .attr("fill", "#e0f3f3")
 
-            const xScale = d3.scaleTime()
-                .domain(d3.extent(dataset, xAccessor))
-                .range([0, dimensions.boundedWidth])
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0, dimensions.boundedWidth])
             
-            const lineGenerator = d3.line()
-            .x(d => xScale(xAccessor(d)))
-            .y(d => yScale(yAccessor(d)))
+    const lineGenerator = d3.line()
+        .x(d => xScale(xAccessor(d)))
+        .y(d => yScale(yAccessor(d)))
 
-            const line = bounds.append("path")
-            .attr("d", lineGenerator(dataset))
-            .attr("fill","none")
-            .attr("stroke", "#af9358")
-            .attr("stroke-width", 2)
+     const line = bounds.append("path")
+        .attr("d", lineGenerator(dataset))
+        .attr("fill","none")
+        .attr("stroke", "#af9358")
+        .attr("stroke-width", 2)
       
 
 }
