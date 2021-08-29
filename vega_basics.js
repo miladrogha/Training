56edb3e4b1db88401007378b032a10fcbd2ed265
Milad Rogha <60856402+miladrogha@users.noreply.github.com>
2021-08-29 16:58:43 -0400

    added basic charts

--- vega_basics.js
+++ vega_basics.js
@@ -1,7 +1,90 @@
-var vlSpec ={
+var vlTick ={
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    width: 1000,
+    align: "center",
+    data:{
+        url: "https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json",
+        async: true
+    },
+    mark:{
+        type: "tick",
+        tooltip: true
+    },
+    encoding:{
+        y: {
+            field: "Miles_per_Gallon",
+            type: "quantitative"
+        },
+        x:{
+            field: "Cylinders",
+            type: "ordinal",
+            axis: {grid: true, tickBand: "extent", labelAngle: 0}
+        }
+    }
+}
+vegaEmbed('#vis0', vlTick);
+
+//Bar chart
+var vlBar ={
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
     align: "center",
     width: 1000,
+    data:{
+        url: "https://raw.githubusercontent.com/vega/vega/master/docs/data/seattle-weather.csv",
+        async: true
+    },
+    mark:{
+        type: "bar",
+        tooltip: true
+    },
+    encoding:{
+        x: {
+            timeUnit:"month",
+            field: "date",
+            type: "temporal"
+        },
+        y:{
+            field: "temp_max",
+            type: "quantitative",
+            aggregate: "mean",
+            scale:{
+                domain: [0,40]
+            }
+        }
+    }
+}
+vegaEmbed('#vis1', vlBar);
+
+//Scatterplot
+var vlScatter ={
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    align: "center",
+    width: 1000,
+    data:{
+        url: "https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json",
+        async: true
+    },
+    mark:{
+        type: "point",
+        tooltip: true
+    },
+    encoding:{
+        y: {
+            field: "Miles_per_Gallon",
+            type: "quantitative"
+        },
+        x:{
+            field: "Horsepower",
+        }
+    }
+}
+vegaEmbed('#vis2', vlScatter);
+
+
+//Multilayer chart
+var vlSpec ={
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    align: "center",
     data:{
         url: "https://raw.githubusercontent.com/vega/vega/master/docs/data/seattle-weather.csv",
         async: true
@@ -70,4 +153,4 @@ var vlSpec ={
     }
     ]
 }
-vegaEmbed('#vis', vlSpec);
+vegaEmbed('#vis8', vlSpec);
