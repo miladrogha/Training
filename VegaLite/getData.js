ab0123c96c3f0095ce9b0399bf25b8ede18e50cc
Milad Rogha <60856402+miladrogha@users.noreply.github.com>
2021-09-13 12:34:31 -0400

    Separation of charts

--- VegaLite/getData.js
+++
@@ -1,9 +0,0 @@
-import {csv} from 'd3';
-
-const csvUrl = "https://raw.githubusercontent.com/nytimes/covid-19-data/e371d1b40449e2b2916955e443ab75fbc235a828/us-states.csv";
-
-export const getData = async () => {
-    const data = await csv(csvUrl);
-    console.log(data[0]);
-    return data;
-};
--- VegaLite/stripPlot.js
+++ VegaLite/stripPlot.js
@@ -1,9 +1,9 @@
-var vlSpec = {
+var stripPlot = {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
     width: (980),
     data: {
       name: "covidUsStates",
-      url: "getData.js"},
+      url: "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"},
     mark:"bar",
     encoding: {
       x: {
@@ -16,25 +16,17 @@ var vlSpec = {
       },
       
       y: {
-          "bin": true,
           aggregate:"sum",
           type: "quantitative",
           field: "deaths",
           title: "Total number of deaths"
         },
       tooltip: [
-        {field: "state", type: "ordinal", title: "State"},
         {field: "cases", type: "quantitative", title: "cases"},
         {field: "deaths", type: "quantitative", title: "deaths"}
-      ],
-      color:{
-          field: "state",
-          type: "nominal",
-          opacity: 20
-      }
+      ]
     }
-  }
-  ;
+  };
 
   // Embed the visualization in the container with id `vis`
-  vegaEmbed('#vis', vlSpec);
+  vegaEmbed('#stripPlot', stripPlot);
